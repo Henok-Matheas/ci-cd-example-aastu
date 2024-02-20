bb6b300bf9be9d01e8ae4e14555b8a0c48d2ea23
Henok-Matheas <henokmatheaskebede@gmail.com>
2024-02-20 17:57:10 +0300

    feat: added documentation for the main function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def greet(name):
     return f"Hello {name}, Docker is running!!"
 
 def main():
+    """Entry point for the application script"""
     name = input("what is your name? ")
     print(greet(name))
 
